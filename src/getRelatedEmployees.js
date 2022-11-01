fdd70862ceb72935943871592136642c5d42ff15
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-01 19:29:56 -0300

    resolvido requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // seu código aqui // Conteúdo retirado https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes#exemplos / apresentado a doc pelo "Lucas Augusto"
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  // Ajuda mentoria vitor para resolução
+  if (isManager(managerId)) {
+    return data.employees.filter((element) => element.managers.includes(managerId))
+      .map((element) => (`${element.firstName} ${element.lastName}`));
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
