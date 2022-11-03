f44a20d28b85688ce35c6230d99411a58e0692c4
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-03 20:45:27 -0300

    resolvido requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
+  // AJUDA DE MENTORIA COM O VITOR
+  if (!animal) {
+    const objectVazio = {};
+    species.forEach((element) => {
+      objectVazio[element.name] = element.residents.length;
+    });
+    return objectVazio;
+  }
+  if (animal.sex) {
+    return species.find((element) => element.name === animal.specie)
+      .residents.filter((element) => element.sex === animal.sex).length;
+  }
+  return species.find((element) => element.name === animal.specie)
+    .residents.length;
 }
-
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
