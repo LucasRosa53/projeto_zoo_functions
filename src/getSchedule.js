67359d20dc79f9f4c6146a6409b2ec370729ed80
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-07 23:04:25 -0300

    resolvido requisito 12 e 13

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (scheduleTarget) {
+    return data.species.find((element) => element.availability);
+  }
 }
 
 module.exports = getSchedule;
